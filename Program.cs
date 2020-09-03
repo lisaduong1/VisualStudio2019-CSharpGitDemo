cd45732f27a5e5d4b63f3e0b63d2c7c8a649db27
Lisa Duong <lisaduong@usf.edu>
2020-09-03 17:10:26 -0400

    Commit the change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace VisualStudio2019_CSharpGitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Here is a change");
         }
     }
 }
